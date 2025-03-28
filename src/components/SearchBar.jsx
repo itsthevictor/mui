b7546e2606d06e7e4f3b7fb1b76c61b0dd068d7c
Victor Alexa <victor.d.alexa@gmail.com>
2025-03-28 13:50:17 +0200

    search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import { Search } from '@mui/icons-material';
+import { Input } from '@mui/material';
 
-const SearchBar = ({ placeholder, onchange }) => {
+const SearchBar = ({ placeholder, onChange, searchBarInput }) => {
   return (
-    <TextField
-      placeholder={placeholder}
-      onChange={onchange}
-      variant='filled'
-      sx={{ width: '100%' }}
-    />
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingTop: '12px',
+        paddingBottom: '12px',
+      }}
+    >
+      <Search
+        sx={{
+          marginRight: '10px',
+          marginLeft: '10px',
+        }}
+      />
+      <TextField
+        placeholder={placeholder}
+        onChange={onChange}
+        variant='standard'
+        fullWidth
+        InputProps={{ disableUnderline: true }}
+      />
+    </Box>
   );
 };
 
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,27 +1,77 @@
-import { Grid2, IconButton, Box } from '@mui/material';
+import { Grid2, IconButton, Box, Typography } from '@mui/material';
 import BasicCard from '../components/BasicCard';
 import SearchBar from '../components/SearchBar';
 import MainButton from '../components/MainButton';
-import { Refresh } from '@mui/icons-material';
+import { Refresh, VerticalAlignCenter } from '@mui/icons-material';
 
 const Auth = () => {
-  const getSearchBar = () => {
+  const searchBarStyles = {
+    container: {
+      display: 'flex',
+      alignItems: 'center',
+      backgroundColor: '#f5f5f5',
+      borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
+      position: 'relative',
+    },
+    actions: {
+      position: 'absolute',
+      right: '0',
+    },
+    content: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '65px',
+      color: 'gray',
+    },
+    searchBarInput: {
+      width: '100%',
+      color: 'rgba(0, 0, 0, 0.6)',
+      fontSize: '1.1rem',
+
+      paddingLeft: '10px',
+      backgroundColor: '#f5f5f5',
+    },
+  };
+  const getContent = () => {
     return (
-      <Box>
+      <Box sx={searchBarStyles.content}>
+        <Typography align='center'>No users for this project yet</Typography>
+      </Box>
+    );
+  };
+  const getSearchBar = () => {
+    const handleChange = (e) => {
+      console.log(e);
+    };
+
+    return (
+      <Box sx={searchBarStyles.container}>
         <SearchBar
+          sx={searchBarStyles.searchBarInput}
           placeholder='Caută numele clientului sau denumirea măsurii'
-          onchange={(e) => handleChange(e.target.value)}
+          onChange={(e) => handleChange(e.target.value)}
         />
-        {/* <MainButton lanbel='oi' /> */}
-        <IconButton>
-          <Refresh />
-        </IconButton>
+        <Box sx={searchBarStyles.actions}>
+          <MainButton
+            children='add user'
+            variant='contained'
+            sx={searchBarStyles.button}
+          />
+          <IconButton>
+            <Refresh />
+          </IconButton>
+        </Box>
       </Box>
     );
   };
   return (
     <Grid2 item xs={8}>
-      <BasicCard header={getSearchBar()} sx={{ width: '100%' }} />
+      <BasicCard
+        header={getSearchBar()}
+        sx={{ maxWidth: '720px', marginLeft: 'auto', marginRight: 'auto' }}
+        content={getContent()}
+      />
     </Grid2>
   );
 };
